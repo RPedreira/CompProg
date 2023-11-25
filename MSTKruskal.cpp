66c9c7f949d9a3707531326406694e780c655ceb
RPedreira <rnpedreira4@gmail.com>
2023-11-25 09:13:50 -0300

    a

--- MSTKruskal.cpp
+++ MSTKruskal.cpp
@@ -3,32 +3,27 @@ int chefe[numVertices], grau[numVertices];
 
 void UFinit(int numVertices)
 {
-    for(int i = 0; i < numVertices; i++)
+    for(int i = 1; i <= numVertices; i++)
     {
         chefe[i] = i;
-        grau[i] = 1;
+        grau[i] = 0;
     }
 }
 
 int UFfind(int vertice)
 {
-    int ans = vertice;
-    while(ans != chefe[ans])
-        ans = chefe[ans];
-    return ans;
+    return (vertice == chefe[vertice]) ? vertice : chefe[vertice] = UFfind(chefe[vertice]);
 }
 
 void UFunion(int x, int y)
 {
     if(grau[x] < grau[y])
-    {
         chefe[x] = y;
-        grau[y] += grau[x];
-    }
     else
     {
         chefe[y] = x;
-        grau[x] += grau[y];
+        if(grau[x] == grau[y])
+            grau[x]++;
     }
 }
 
@@ -36,17 +31,17 @@ int MSTKruskal()
 {
     int x, y, z, cont = 1, ans = 0;
     sort(pesos, pesos + numArestas);
-        for(int i = 0; i < numArestas && cont < numVertices; i++)
+    for(int i = 0; i < numArestas && cont < numVertices; i++)
+    {
+        z = pesos[i].second;
+        x = UFfind(arestas[z].first);
+        y = UFfind(arestas[z].second);
+        if(x != y)
         {
-            z = pesos[i].second;
-            x = UFfind(arestas[z].first);
-            y = UFfind(arestas[z].second);
-            if(x != y)
-            {
-                ans += pesos[i].first;
-                cont++;
-                UFunion(x, y);
-            }
+            ans += pesos[i].first;
+            cont++;
+            UFunion(x, y);
         }
-        return ans;
+    }
+    return ans;
 }
